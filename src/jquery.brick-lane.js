a50f815630cbfa672c4ee3cdc7cf73ca4a48e5ea
Nicholas Valbusa <squallstar@gmail.com>
2014-10-01 00:52:36 +0100

    examples and readme file

--- src/jquery.brick-lane.js
+++ src/jquery.brick-lane.js
@@ -2,7 +2,7 @@
  * Brick Lane v0.0.1
  * Hipstery Cascading Grid Layout Library
  * MIT License
- * by Nicholas Valbusa & Paolo Moretti
+ * by Nicholas Valbusa
  */
 
 ( function( window, $ ) {
@@ -55,7 +55,10 @@
       columnWidth: 'auto',
 
       /* The jQuery selector to specify the children elements */
-      itemSelector: undefined
+      itemSelector: undefined,
+
+      /* Binds layout to window resizing */
+      isResizeBound: true
 
     }, options);
 
@@ -70,14 +73,12 @@
       colYs = [],
       containerWidth = undefined,
       initialized = false,
+      resizeTimeout = undefined
 
       _init = function() {
         if (initialized) {
           _cleanup();
         }
-
-        containerWidth = $el.width();
-        console.log('containerWidth', containerWidth);
 
         $el.css('overflow', 'hidden');
         if ($el.css('position') == 'static') {
@@ -104,26 +105,68 @@
           };
         }
 
-        _bindViewportSize();
+        _onViewportResize(0);
+
+        if (settings.isResizeBound) {
+          _bindResize();
+        }
 
         initialized = true;
-
-        $elements.each(function() {
-          _layoutElement( $(this), false );
-        });
-
-        _adjustViewportHeight();
       },
 
       _cleanup = function() {
         colYs = [];
       },
 
+      _destroy = function() {
+        _unbindResize();
+
+        $el.removeAttr('style');
+
+        $elements.each(function() {
+          $(this).removeAttr('style');
+        });
+      },
+
+      _onViewportResize = function(delay) {
+        if (resizeTimeout) {
+          window.clearTimeout(resizeTimeout);
+          delete resizeTimeout;
+        }
+
+        window.setTimeout(function() {
+          var w = $el.width();
+          console.log(w, containerWidth);
+          if (w !== containerWidth) {
+            containerWidth = w;
+            _setViewportSize();
+            _layoutElements();
+          }
+        }, delay ? delay : 50);
+      },
+
+      _bindResize = function() {
+        $(window).on("resize", _onViewportResize);
+      },
+
+      _unbindResize = function() {
+        $(window).off("resize", _onViewportResize);
+      },
+
       _addElement = function( $element ) {
         $el.append( $element );
+        _layoutElement( $element );
+      },
+
+      _layoutElements = function() {
+        $elements.each(function() {
+          _layoutElement( $(this) );
+        });
+
+        _adjustViewportHeight();
       }
 
-      _layoutElement = function( $element, adjustContainerHeight ) {
+      _layoutElement = function( $element ) {
         var width = $element.outerWidth(),
             height = $element.outerHeight(true);
 
@@ -150,15 +193,16 @@
 
         console.log('new columns heights', colYs);
 
-        $element.css({
-          position: 'absolute',
-          left: position.x,
+        data = {
+          left : position.x,
           top: position.y
-        });
+        };
 
-        if (adjustContainerHeight !== false) {
-          _adjustViewportHeight();
+        if ($element.css('position') !== 'absolute') {
+          data.position = 'absolute';
         }
+
+        $element.css(data);
       },
 
       _adjustViewportHeight = function() {
@@ -166,15 +210,15 @@
         $el.css('height', maxY);
       }
 
-      _bindViewportSize = function() {
+      _setViewportSize = function() {
         var w = columnWidth();
         columnsCount = Math.floor(containerWidth / w);
         console.log('columns count', columnsCount);
 
+        colYs = [];
+
         for (var i = 0; i < columnsCount; i++) {
-          if (!colYs[i]) {
-            colYs.push(0);
-          }
+          colYs.push(0);
         };
       };
 
@@ -189,6 +233,14 @@
 
         appended: function ( $element ) {
           _appendedElement( $element );
+        },
+
+        append: function ( $element ) {
+          _addElement( $element );
+        },
+
+        destroy: function (){
+          _destroy();
         }
       }
     };
