f99cdb2fb903be06c69fbdfc33696f68010e40a5
Nicholas Valbusa <squallstar@gmail.com>
2014-09-30 18:09:14 +0100

    plugin skeleton

---
+++ src/jquery.brick-lane.js
@@ -0,0 +1,22 @@
+/*!
+ * Brick Lane v0.0.1
+ * Hipstery Cascading Grid Layout Library
+ * MIT License
+ * by Nicholas Valbusa & Paolo Moretti
+ */
+
+( function( window, $ ) {
+
+  $.fn.brickLane = function(options) {
+
+    var settings = $.extend({
+      columnWidth: 200
+    }, options );
+
+    return this.each(function() {
+      // ...
+    });
+
+  };
+
+})( window, jQuery );
